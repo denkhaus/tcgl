22d28c2fda828b7eebcb1a0da011f438bd4140b2
Frank Mueller <themue@gmail.com>
2012-11-19 10:08:58 +0100

    Reverted removed event context, has not been intended.

--- cells/cells.go
+++ cells/cells.go
@@ -33,6 +33,10 @@ type Event interface {
 	Topic() string
 	// Payload returns the payload of the simple event.
 	Payload() interface{}
+	// Context returns the context of a set of event processings.
+	Context() *Context
+	// SetContext set the context of a set of event processings.
+	SetContext(c *Context)
 }
 
 // simpleEvent can be used if no own event implementation is
